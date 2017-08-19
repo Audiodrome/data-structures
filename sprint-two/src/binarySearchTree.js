c81e94d1904da62d98a454626e6e0f745e7c8a93
Hack Reactor Student <student@hackreactor.com>
2017-08-18 20:56:17 -0700

    save

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,32 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.root = null;
+
+  tree.insert = function(key, value) {
+    if (this.root === null) {
+      this.root = Node();
+    } else {
+
+    }
+
+  };
+  
+  tree.contains = function(key) {
+  };
+
+  tree.depthFirstLog = function() {
+  };
 };
 
 
+
+var TreeNode = function(key, value) {
+  var node = {};
+  node.key = key;
+  node.value = value;
+  node.right = null;
+  node.left = null;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -46,4 +46,3 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
-
